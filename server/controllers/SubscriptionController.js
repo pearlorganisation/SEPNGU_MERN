d35c9ba05f3bf006fab4638f70e64e2072ae5bb0
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-02-27 12:20:46 +0530

    Add subscription model and controller, implement email and phone storage for subscriptions

--- server/controllers/SubscriptionController.js
+++ server/controllers/SubscriptionController.js
@@ -1,14 +1,25 @@
 import { razorpayInstance } from "../configs/razorpay.js";
 
 export const createSubcription = async (req, res) => {
-  const { planId } = req.body;
+  const { planId, email, phone } = req.body;
   try {
     const subscription = await razorpayInstance.subscriptions.create({
       plan_id: planId,
       total_count: 1, // Single 20-day period: total number of billing cycles
       customer_notify: 1,
     });
-    console.log(subscription);
+    // console.log(subscription);
+    // Store Subscription WITHOUT a userId (since user isn't logged in)
+    const newSubscription = new Subscription({
+      subscriptionId: razorpayResponse.id,
+      planId,
+      status: "created",
+      email, // Temporarily store email
+      phone,
+      startDate: new Date(),
+      endDate: new Date(Date.now() + 3 * 30 * 24 * 60 * 60 * 1000),
+    });
+    await newSubscription.save();
     res.status(201).json({
       success: true,
       message: "Subscription created successfully",
