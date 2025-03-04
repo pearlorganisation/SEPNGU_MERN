b26a27515e23b8de425f928a3cfcfceb8e506198
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-03-04 12:02:35 +0530

    Update API host configuration to use environment variable for flexibility

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const HOST = "http://localhost:3005";
+const HOST = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3005";
 
 const AUTH_ROUTE = `${HOST}/api/auth`;
 const MESSAGES_ROUTE = `${HOST}/api/messages`;
