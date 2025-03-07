c8e498a24f5445499cd8fcfd00f03d2fbcec63cb
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-03-07 10:18:14 +0530

    Remove obsolete migration files and clean up database schema

--- server/prisma/migrations/20250303122049_changed_plan_id_type/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Subscription" DROP CONSTRAINT "Subscription_planId_fkey";
-
--- AlterTable
-ALTER TABLE "Subscription" ALTER COLUMN "planId" SET DATA TYPE TEXT;
-
--- AddForeignKey
-ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_planId_fkey" FOREIGN KEY ("planId") REFERENCES "Plan"("planId") ON DELETE RESTRICT ON UPDATE CASCADE;
